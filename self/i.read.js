47df82fd2c1cd81627c9f1f5824627f6ccc5e852
Shineslee <46121976+Shineslee@users.noreply.github.com>
2021-06-09 22:35:12 +0800

    Update i.read.js

--- self/i.read.js
+++ self/i.read.js
@@ -1,22 +1,2 @@
-//body = $response.body.replace(/expiredTime":\d+, 'expiredTime":2998743249).replace(/expired":\d+, 'expired":0').replace(/remainTime":\d+, 'remainTime":2998743249');
-//$done({body});
-/*
-
-QX:
-[rewrite_local]
-#解锁会员
-^https:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary url script-response-body https://raw.githubusercontent.com/Shineslee/rules/master/self/i.read.js
-
-[mitm]
-hostname = i.weread.qq.com,
-
-*/
-
-let obj = JSON.parse($response.body);
-obj = {
-  "expiredTime": 2998743249,
-  "expired": 0,
-  "remainTime": 2998743249,
- };
-
-$done({body: JSON.stringify(obj)});
+body = $response.body.replace("expiredTime":\d+, "expiredTime":2998743249).replace("expired":\d+, "expired":0').replace（"remainTime":\d+, "remainTime":2998743249');
+$done({body});
