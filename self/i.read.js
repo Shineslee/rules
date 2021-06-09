b4554ddd4b5724b8ad09ba104e318bf80d8bade6
Shineslee <46121976+Shineslee@users.noreply.github.com>
2021-06-10 00:08:16 +0800

    Update i.read.js

--- self/i.read.js
+++ self/i.read.js
@@ -2,14 +2,22 @@
 //body = $response.body.replace("expiredTime":\d+, "expiredTime":2998743249).replace("expired":\d+, "expired":0).replace（"remainTime":\d+, "remainTime":2998743249);
 //$done({body});
 
-body = $response.body;
-
-let obj = JSON.parse(body);
-obj.expiredTime=2998743249;
-obj.expired=0;
-obj.isPaying=1;
-obj.remainTime=2998743249;
-
-body = JSON.stringify(obj);
-
-$done(body);
+let obj = JSON.parse($response.body);
+obj = {
+  "expiredTime": 2998743249,
+  "expired": 0,
+  "isPaying": 1,
+  "remainTime": 2998743249,
+  "payingRemainTime": 0,
+  "isAutoRenewable": 0,
+  "historyAutoRenewable": 0,
+  "autoRenewableChannel": 0,
+  "autoRenewableTime": 0,
+  "autoRenewablePrice": 1900,
+  "savedMoney": 22266,
+  "totalFreeReadDay": 0,
+  "remainCoupon": 0,
+  "remainCount": 0 
+ };
+ 
+ $done({body: JSON.stringify(obj)});
