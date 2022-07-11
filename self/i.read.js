63b916c73a9b63809f0f5a179d386aa3081f9547
Shineslee <46121976+Shineslee@users.noreply.github.com>
2022-07-11 13:57:16 +0800

    Update i.read.js

--- self/i.read.js
+++ self/i.read.js
@@ -1,23 +1,20 @@
+/*微信阅读
+[Script]
+^https?:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary
+[MITM]
+hostname = i.weread.qq.com
+*/
 
-//body = $response.body.replace("expiredTime":\d+, "expiredTime":2998743249).replace("expired":\d+, "expired":0).replace（"remainTime":\d+, "remainTime":2998743249);
-//$done({body});
-
-let obj = JSON.parse($response.body);
-obj = {
-  "expiredTime":2998743249,
-  "expired":0,
-  "isPaying":1,
-  "remainTime":2998743249,
-  "payingRemainTime": 0,
-  "isAutoRenewable": 0,
-  "historyAutoRenewable": 0,
-  "autoRenewableChannel": 0,
-  "autoRenewableTime": 0,
-  "autoRenewablePrice": 1900,
-  "savedMoney": 22266,
-  "totalFreeReadDay": 0,
-  "remainCoupon": 0,
-  "remainCount": 0 
- };
- 
- $done({body: JSON.stringify(obj)});
+var body = $respone;
+var url = $request.url;
+const path = "/pay/memberCardSummary";
+let obj = JSON.parse(body);
+if (url.indexOf(path) != -1) {
+	obj.expiredTime = 2998743249;
+	obj.expired = 0;
+	obj.remainTime = 189302400;
+	obj.isPaying = 1;
+	
+	body = JSON.stringify(obj);
+}
+$done({body});
